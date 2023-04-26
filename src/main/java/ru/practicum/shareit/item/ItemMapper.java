e9257f6ce238362fe92b23c5a748f9a3de8a7025
Gleb Zakharov <webmaster-80@yandex.ru>
2023-04-26 12:20:12 +0300

    feat: добавить mapper

---
+++ src/main/java/ru/practicum/shareit/item/ItemMapper.java
@@ -0,0 +1,14 @@
+package ru.practicum.shareit.item;
+
+import ru.practicum.shareit.item.dto.ItemDto;
+
+public class ItemMapper {
+    public static ItemDto toItemDto(Item item) {
+        return ItemDto.builder()
+                .id(item.getId())
+                .name(item.getName())
+                .description(item.getDescription())
+                .available(item.isAvailable())
+                .build();
+    }
+}
